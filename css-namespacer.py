b50b1ce6dbf51b4903532ad5ef36b6eb3938bf18
Alexander Solovyov <alexander@solovyov.net>
2012-01-05 01:02:30 +0200

    fix namespacer

--- css-namespacer.py
+++ css-namespacer.py
@@ -3,7 +3,7 @@
 import sys
 import cssutils
 
-PREFIX = 'div#_showkr'
+PREFIX = '.showkr'
 cssutils.log.enabled = False
 # cssutils.ser.prefs.keepComments = False
 # cssutils.ser.prefs.indent = ''
@@ -12,13 +12,17 @@ cssutils.log.enabled = False
 # cssutils.ser.prefs.propertyNameSpacer = ''
 
 def main(fn):
-    style = cssutils.parseFile(fn)
+    style = cssutils.parseString(open(fn).read())
     for rule in style.cssRules:
         if not hasattr(rule, 'selectorList'):
             continue
-        for selector in rule.selectorList:
-            selector.selectorText = PREFIX + ' ' + selector.selectorText
-    print style.cssText
+        for s in rule.selectorList:
+            if s.selectorText in ('html', 'body'):
+                s.selectorText = PREFIX
+            else:
+                s.selectorText = PREFIX + ' ' + s.selectorText
+    text = style.cssText
+    print text.replace('\x00', '\\0')
 
 
 if __name__ == '__main__':
--- embed.coffee
+++ embed.coffee
@@ -2,7 +2,7 @@
     id = ("showkr" + Math.random()).replace('.', '');
     document.write("<link rel='stylesheet' href='namespaced.css'>
     <script src='app.js'></script>
-    <div id='_showkr'><div class='container'><div class='row'><div id='#{id}' class='span16'></div></div></div></div>
+    <div class='showkr'><div class='container'><div class='row'><div id='#{id}' class='span16'></div></div></div></div>
     <script>
     window.app = new Showkr('##{id}', $('script[data-set]').attr('data-set'));
     </script>
