8a2dd76a743aaf16272b156f38b6beefe1c2bcf4
Alexander Solovyov <alexander@solovyov.net>
2014-08-01 11:05:22 +0300

    cleanup showkr.data

--- src/showkr/data.cljs
+++ src/showkr/data.cljs
@@ -15,9 +15,21 @@
 (defn- flickr-error [payload]
   (js/console.error "Error fetching data with parameters:" payload))
 
-(defn call-flickr [payload callback]
+(defn flickr-call [payload callback]
   (let [q (Jsonp. URL "jsoncallback")]
     (.send q (clj->js (merge OPTS payload)) callback flickr-error)))
+
+(defn flickr-fetch [path attr payload & [cb]]
+  (when-not (get-in @world path)
+    (swap! world assoc-in path ^{:state :waiting} {})
+    (flickr-call payload
+      (fn [data]
+        (let [data (js->clj data :keywordize-keys true)]
+          (swap! world assoc-in path
+            (condp = (:stat data)
+              "ok"   (with-meta (attr data) {:state :fetched})
+              "fail" (with-meta data {:state :failed})))
+          (when cb (cb)))))))
 
 (defn fetched? [data]
   (= :fetched (:state (meta data))))
@@ -25,53 +37,29 @@
 ;;; A-la flux or something, call it and data will appear
 
 (defn fetch-set [id]
-  (when-not (get-in @world [:data :sets id])
-    (swap! world assoc-in [:data :sets id]
-      ^{:state :waiting} {})
-    (call-flickr {:method "flickr.photosets.getPhotos"
-                  :photoset_id id
-                  :extras "original_format,description,path_alias"}
-      (fn [data]
-        (swap! world assoc-in [:data :sets id]
-          (with-meta (js->clj (aget data "photoset") :keywordize-keys true)
-            {:state :fetched}))))))
+  (flickr-fetch [:data :sets id] :photoset
+    {:method "flickr.photosets.getPhotos"
+     :photoset_id id
+     :extras "original_format,description,path_alias"}))
 
 (defn fetch-comments [set-id idx]
-  (let [photo (get-in @world [:data :sets set-id :photo idx])]
-    (when (and photo (not (:comments photo)))
-      (swap! world assoc-in [:data :sets set-id :photo idx :comments]
-        ^{:state :waiting} {})
-      (call-flickr {:method "flickr.photos.comments.getList"
-                    :photo_id (:id photo)}
-        (fn [data]
-          (swap! world assoc-in [:data :sets set-id :photo idx :comments]
-            (with-meta (js->clj (aget data "comments") :keywordize-keys true)
-              {:state :fetched})))))))
+  (when (fetched? (get-in @world [:data :sets set-id]))
+    (flickr-fetch [:data :sets set-id :photo idx :comments] :comments
+      {:method "flickr.photos.comments.getList"
+       :photo_id (get-in @world [:data :sets set-id :photo idx :id])})))
 
 (defn fetch-user-sets [username]
   (let [user (get-in @world [:data :users username])]
-    (when (and (fetched? user) (not (:sets user)))
-      (swap! world assoc-in [:data :users username :sets]
-        ^{:state :waiting} {})
-      (call-flickr {:method "flickr.photosets.getList"
-                    :user_id (:id user)}
-        (fn [data]
-          (swap! world assoc-in [:data :users username :sets]
-            (with-meta (js->clj (aget data "photosets") :keywordize-keys true)
-              {:state :fetched})))))))
+    (when (fetched? user)
+      (flickr-fetch [:data :users username :sets] :photosets
+        {:method "flickr.photosets.getList"
+         :user_id (:id user)}))))
 
 (defn fetch-user [username]
-  (when-not (get-in @world [:data :users username])
-    (swap! world assoc-in [:data :users username]
-      ^{:state :waiting} {})
-    (call-flickr {:method "flickr.urls.lookupUser"
-                  :url (str "https://flickr.com/photos/" username)}
-      (fn [data]
-        (swap! world assoc-in [:data :users username]
-          (with-meta (js->clj (aget data "user") :keywordize-keys true)
-            {:state :fetched}))
-        (fetch-user-sets username)
-        #_ (fetch-user-info username)))))
+  (flickr-fetch [:data :users username] :user
+    {:method "flickr.urls.lookupUser"
+     :url (str "https://flickr.com/photos/" username)}
+    #(fetch-user-sets username)))
 
 ;;; not sure this is the right place
 
